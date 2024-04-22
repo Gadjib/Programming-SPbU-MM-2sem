bef29eb31a354d27a03cc5a52647e3551ac0604b
Ilya <ilya@andrianov.ru>
2024-04-22 18:03:46 +0300

    Прежнее состояние из VS с дотнетом 4

---
+++ Stack Calculator/ArrayStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Calculator
+{
+    public class ArrayStack : Stack
+    {
+        private int[] array;
+        private int top;
+
+        public ArrayStack(int capacity)
+        {
+            array = new int[capacity];
+            top = -1;
+        }
+
+        public void Push(int value)
+        {
+            if (top == array.Length - 1)
+            {
+                throw new StackOverflowException("Stack overflow");
+            }
+
+            array[++top] = value;
+        }
+
+        public int Pop()
+        {
+            if (top == -1)
+            {
+                throw new InvalidOperationException("Stack underflow");
+            }
+
+            return array[top--];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+    }
+}
---
+++ Stack Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Calculator;
+
+class Program
+{
+    static void Main() 
+    {
+        int stackSize = 100;
+        ArrayStack stack = new ArrayStack(stackSize);
+        string str = "1 2 + 3 *";
+        double answer = StackCalculator.Calculate(str, stack);
+        Console.WriteLine(answer);
+    }
+}
+
