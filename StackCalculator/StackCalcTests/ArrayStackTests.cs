3f646d6e8a636537ff35725870794b173d3a8ee3
ilya <ilya@andrianov.ru>
2025-02-03 22:01:25 +0300

    Добавил новое

---
+++ StackCalculator/StackCalcTests/ArrayStackTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stack_Calculator;
+
+namespace StackCalcTests
+{
+    [TestClass]
+    public class ArrayStackTests
+    {
+        [TestMethod]
+        public void TestCalculator()
+        {
+            string string1 = "1 2 + 3 *";
+            string string2 = "1 1 +";
+            int correctNumber1 = 9;
+            int correctNumber2 = 2;
+
+            int stackSize = 100;
+            ArrayStack stack = new ArrayStack(stackSize);
+            
+            double number1 = StackCalculator.Calculate(string1, stack);
+            double number2 = StackCalculator.Calculate(string2, stack);
+            Assert.AreEqual(correctNumber1, number1);
+            Assert.AreEqual(correctNumber2, number2);
+        }
+    }
+}
---
+++ StackCalculator/StackCalcTests/ListStackTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stack_Calculator;
+
+namespace StackCalcTests
+{
+    [TestClass]
+    public class ListStackTests
+    {
+        [TestMethod]
+        public void TestCalculator()
+        {
+            string string1 = "1 2 + 3 *";
+            string string2 = "1 1 +";
+            int correctNumber1 = 9;
+            int correctNumber2 = 2;
+
+            int stackSize = 100;
+            ListStack stack = new ListStack(stackSize);
+
+            double number1 = StackCalculator.Calculate(string1, stack);
+            double number2 = StackCalculator.Calculate(string2, stack);
+            Assert.AreEqual(correctNumber1, number1);
+            Assert.AreEqual(correctNumber2, number2);
+        }
+    }
+}
